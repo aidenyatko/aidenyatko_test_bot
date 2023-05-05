ff53b8b69769224fbb2d3502adfcdfe979fbba0f
aidenyatko <iamnotkisell@gmail.com>
2023-05-06 02:01:01 +0300

    bot answer to all update types

--- echo_bot.py
+++ echo_bot.py
@@ -1,8 +1,5 @@
 from aiogram import Bot, Dispatcher
-from aiogram.filters import Command
 from aiogram.types import Message
-from aiogram.types import ContentType
-from aiogram import F
 from environs import Env
 
 # Получаем токен
@@ -26,28 +23,16 @@ async def process_help_command(message: Message):
                          'я пришлю тебе твое сообщение')
 
 
-# Этот хэндлер будет срабатывать на отправку боту фото
-async def send_photo_echo(message: Message):
-    print(message)
-    await message.reply_photo(message.photo[0].file_id)
-
-async def send_audio_echo(message: Message):
-    print(message)
-    await message.reply_audio(message.audio.file_id)
-
-
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
+@dp.message()
 async def send_echo(message: Message):
-    await message.reply(text=message.text)
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(text='Данный тип апдейтов не поддерживается '
+                                 'методом send_copy')
 
-
-# Регистрируем хэндлеры
-dp.message.register(process_start_command, Command(commands=["start"]))
-dp.message.register(process_help_command, Command(commands=['help']))
-dp.message.register(send_photo_echo, F.content_type == ContentType.PHOTO)
-dp.message.register(send_audio_echo, F.content_type == ContentType.AUDIO)
-dp.message.register(send_echo)
 
 if __name__ == '__main__':
     dp.run_polling(bot)
