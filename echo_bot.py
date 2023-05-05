218b57e09af4837ffe16274461d181165f891704
aidenyatko <iamnotkisell@gmail.com>
2023-05-06 01:27:41 +0300

    add handler registers

--- echo_bot.py
+++ echo_bot.py
@@ -1,12 +1,10 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
-from environs import Env
 
-# Получаем токен
-env = Env()
-env.read_env('.env')
-API_TOKEN: str = env.str("BOT_TOKEN")
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
+# полученный у @BotFather
+API_TOKEN: str = 'BOT TOKEN HERE'
 
 # Создаем объекты бота и диспетчера
 bot: Bot = Bot(token=API_TOKEN)
@@ -14,13 +12,11 @@ dp: Dispatcher = Dispatcher()
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
-@dp.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
-@dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer('Напиши мне что-нибудь и в ответ '
                          'я пришлю тебе твое сообщение')
@@ -28,10 +24,14 @@ async def process_help_command(message: Message):
 
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
-@dp.message()
 async def send_echo(message: Message):
     await message.reply(text=message.text)
 
+
+# Регистрируем хэндлеры
+dp.message.register(process_start_command, Command(commands=["start"]))
+dp.message.register(process_help_command, Command(commands=['help']))
+dp.message.register(send_echo)
 
 if __name__ == '__main__':
     dp.run_polling(bot)
