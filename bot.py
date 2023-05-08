7af32c320c888f7b6778dff9955705199b294e9c
aidenyatko <iamnotkisell@gmail.com>
2023-05-08 23:35:19 +0300

    complete module echo bot using new template

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.storage.redis import RedisStorage, DefaultKeyBuilder
 
-from tgbot.config import load_config
+from tgbot.config import Config, load_config
 from tgbot.handlers.admin import admin_router
 from tgbot.handlers.echo import echo_router
 from tgbot.handlers.user import user_router
@@ -33,9 +33,10 @@ async def main():
         format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
     )
     logger.info("Starting bot")
-    config = load_config(".env")
+    config: Config = load_config(".env")
     if config.tg_bot.use_redis:
-        storage = RedisStorage.from_url(config.redis.dsn(), key_builder=DefaultKeyBuilder(with_bot_id=True, with_destiny=True))
+        storage = RedisStorage.from_url(config.redis.dsn(),
+                                        key_builder=DefaultKeyBuilder(with_bot_id=True, with_destiny=True))
     else:
         storage = MemoryStorage()
     bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
@@ -51,6 +52,7 @@ async def main():
     register_global_middlewares(dp, config)
 
     await on_startup(bot, config.tg_bot.admin_ids)
+    # await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 
--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -1,10 +1,24 @@
 from aiogram import Router
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
+from tgbot.lexicon.lexicon_ua import LEXICON_UA
 
 user_router = Router()
 
 
 @user_router.message(CommandStart())
 async def user_start(message: Message):
-    await message.reply("Вітаю, звичайний користувач!")
+    await message.reply(text=LEXICON_UA['/start'])
+
+
+@user_router.message(Command(commands='help'))
+async def user_help(message: Message):
+    await message.reply(text=LEXICON_UA['/help'])
+
+
+@user_router.message()
+async def bot_echo(message: Message):
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(text=LEXICON_UA['no_echo'])
